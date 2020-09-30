7d4b0a7879926697ea6aef25c026c8e7da6390c8
Heather <heather.craddock@hotmail.co.uk>
2020-09-30 12:27:39 -0400

    solution

--- src/main/java/cd/get/ready/algorithms/Fibonacci.java
+++ src/main/java/cd/get/ready/algorithms/Fibonacci.java
@@ -2,6 +2,12 @@ package cd.get.ready.algorithms;
 
 public class Fibonacci {
     public int getFibonacciValue(int n){
-        return -1;
+        if (n==0) {
+            return 0;
+        } else if (n==1){
+            return 1;
+        } else {
+            return getFibonacciValue(n-1)+getFibonacciValue(n-2);
+        }
     }
 }
